87ef9a064968bb3ad74d133225e85b24979f317c
TaffarelXavier <taffarel_deus@hotmail.com>
2018-11-01 13:20:26 -0300

    Questão 9) Faça um programa que receba um valor que foi depositado e exiba o valor com rendimento após 4 meses.

---
+++ src/algoritimo_2018_2/exercicio_1/Questao_9.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritimo_2018_2.exercicio_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Taffrel Xavier <taffarel_deus@hotmail.com>
+ */
+public class Questao_9 {
+
+    public static void main(String[] args) {
+        double valorDeposito;
+
+        valorDeposito = Double.parseDouble(JOptionPane.showInputDialog(null, ""));
+
+        System.out.println(valorDeposito);
+    }
+}
