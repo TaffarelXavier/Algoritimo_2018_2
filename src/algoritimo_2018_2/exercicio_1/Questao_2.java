99093b8ef3ed48fe4be65135ac7847f9d718089a
TaffarelXavier <taffarel_deus@hotmail.com>
2018-11-01 11:18:56 -0300

    Questão 2

--- src/algoritimo_2018_2/exercicio_1/Questao_2.java
+++ src/algoritimo_2018_2/exercicio_1/Questao_2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package algoritimo_2018_2.exercicio_1;
 
 import javax.swing.JOptionPane;
