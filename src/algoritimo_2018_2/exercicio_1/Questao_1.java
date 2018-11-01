b6178d2dc10813ced3b195dd1a2547d991b5f246
TaffarelXavier <taffarel_deus@hotmail.com>
2018-11-01 09:18:41 -0300

    Arquivo index do PHP

--- src/algoritimo_2018_2/exercicio_1/Questao_1.java
+++ src/algoritimo_2018_2/exercicio_1/Questao_1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algoritimo_2018_2.exercicio_1;
 
 import java.util.Arrays;
@@ -14,12 +9,12 @@ import javax.swing.JOptionPane;
  */
 public class Questao_1 {
 
-    public static double[] ordernarArray(double[] array) { //sort in descending order
+    public static int[] ordernarArray(int[] array) { //sort in descending order
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if (array[i] >= array[j]) {
-                    double x = array[i];
+                    int x = array[i];
                     array[i] = array[j];
                     array[j] = x;
                 }
@@ -29,18 +24,35 @@ public class Questao_1 {
     }
 
     /**
+     *
+     * @param number
+     * @return
+     */
+    public static boolean isInteger(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+
+        }
+        return false;
+    }
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        double n[] = new double[4];
+        int n[] = new int[4];
 
         for (int i = 0; i < n.length; i++) {
-            try {
-                n[i] = Integer.parseInt(JOptionPane.showInputDialog(null, ""));
-            } catch (Exception e) {
-            }
 
+            while (n[i] == 0) {
+                try {
+                    n[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Número:" + (i + 1)));
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Insira um valor correto para o número: "  + (i + 1));
+                }
+            }
         }
 
         ordernarArray(n);
