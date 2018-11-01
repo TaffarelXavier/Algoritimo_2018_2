dc79c462eb8f6a1b5dad2603dbd9d1b1555af0a3
TaffarelXavier <taffarel_deus@hotmail.com>
2018-11-01 12:16:58 -0300

    Questão 6

---
+++ src/algoritimo_2018_2/exercicio_1/Questao_6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritimo_2018_2.exercicio_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Taffarel Xavier <taffarel_deus@hotmail.com>
+ */
+public class Questao_6 {
+
+    public static void main(String[] args) {
+        String a, b, c, d;
+
+        a = JOptionPane.showInputDialog(null, "Digite algum valoar para variável A");
+        c = a;
+        b = JOptionPane.showInputDialog(null, "Digite algum valoar para variável B");
+        d = b;
+        
+        a = d;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
